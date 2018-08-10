7f45f62b6e30ad2f4f270dbc806db51200b44821
ucha <uchachaduneli@gmail.com>
2018-08-10 22:24:34 +0400

    asdas

---
+++ src/main/java/ge/tvera/model/AbonentPackageHistory.java
@@ -0,0 +1,98 @@
+package ge.tvera.model;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "abonent_package_history", schema = "tvera")
+public class AbonentPackageHistory {
+    private Integer id;
+    private Abonent abonent;
+    private Package packages;
+    private Timestamp createDate;
+    private Double juridicalPrice;
+    private Double phisicalPrice;
+    private Users user;
+
+    public AbonentPackageHistory(Abonent abonent, Package packages, Double juridicalPrice, Double phisicalPrice, Users user) {
+        this.abonent = abonent;
+        this.packages = packages;
+        this.juridicalPrice = juridicalPrice;
+        this.phisicalPrice = phisicalPrice;
+        this.user = user;
+    }
+
+    public AbonentPackageHistory() {
+    }
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JoinColumn(name = "abonent_id")
+    @OneToOne
+    public Abonent getAbonent() {
+        return abonent;
+    }
+
+    public void setAbonent(Abonent abonent) {
+        this.abonent = abonent;
+    }
+
+    @JoinColumn(name = "package_id")
+    @OneToOne
+    public Package getPackages() {
+        return packages;
+    }
+
+    public void setPackages(Package packages) {
+        this.packages = packages;
+    }
+
+    @Basic
+    @Column(name = "create_date", nullable = false, updatable = false, insertable = false)
+    public Timestamp getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Timestamp createDate) {
+        this.createDate = createDate;
+    }
+
+    @Basic
+    @Column(name = "juridical_price", nullable = true, precision = 0)
+    public Double getJuridicalPrice() {
+        return juridicalPrice;
+    }
+
+    public void setJuridicalPrice(Double juridicalPrice) {
+        this.juridicalPrice = juridicalPrice;
+    }
+
+    @Basic
+    @Column(name = "phisical_price", nullable = true, precision = 0)
+    public Double getPhisicalPrice() {
+        return phisicalPrice;
+    }
+
+    public void setPhisicalPrice(Double phisicalPrice) {
+        this.phisicalPrice = phisicalPrice;
+    }
+
+    @JoinColumn(name = "user_id")
+    @OneToOne
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+}
